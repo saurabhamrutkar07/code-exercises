7b9df9a03ed7da29b24cfcdd1e66c1c50eee97b8
saurabhAmrutkar01 <saurabh.amrutkar@1silverbullet.tech>
2026-01-21 03:17:29 +0530

    one file skips earlier

--- Logic_Building_From_Scratch/Phase1/Level1/01.py
+++ Logic_Building_From_Scratch/Phase1/Level1/01.py
@@ -1,22 +1,20 @@
 """
-Take a number and print wherather its positive, negative or zero 
-
+Take a number and print whether it is positive, negative or zero
 """
 
-try : 
-    user_input = input("Enter the the number : ").strip()
+try: 
+    user_input = input("Enter the number: ").strip()
 
     if user_input == "":
-        print("Enter valid integer")
-    
+        print("Enter a valid number")
     else:
         number = float(user_input)
-        if number > 0 :
+        if number > 0:
             print("positive")
-        elif number < 0 :
+        elif number < 0:
             print("negative")
         else:
             print("zero")
 
-except ValueError as e :
+except ValueError as e:
     print(f"{e}")
